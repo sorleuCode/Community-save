02308df1e28f50b8895f72c335e83090132ca1bf
basirat10 <basiratamusat10@gmail.com>
2024-07-04 14:28:56 +0100

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+// import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { PaymentProvider } from './Context/PaymentContext';
 import Login from './Components/Auth/Login';
 import Register from './Components/Auth/Register';
 import Dashboard from './Components/Dashboard/Dashboard';
+import Recover from './Components/Auth/Recover';
+
 
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         
         <Routes>
           <Route path="/login" element={<Login/>} />
+          <Route path="/recover" element={<Recover/>} />
+
           <Route path="/register" element={<Register/>} />
           {/* <PrivateRoute path="/dashboard" element={Dashboard} /> */}
           <Route path="/dashboard" element={<Dashboard/>} />
