3f7e4f9691c3b64f8409fa76f074a9d381eb2902
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-07-09 15:16:29 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import React from 'react';
-import Dashboard from './Components/Dashboard/Dashboard';
-import Homepage from './pages/Homepage';
-import PaymentHistory from './Components/Dashboard/PaymentHistory';
-import DashboardPage from './pages/DashboardPage';
-import { PaymentProvider } from './Context/PaymentContext';
-import { Route, Routes } from 'react-router-dom';
-import Login from "../src/Components/Auth/Login"
-import Register from "../src/Components/Auth/Register"
-
+import React from 'react'
 
 const App = () => {
   return (
-    <>
-      <PaymentProvider>
-        
-        <Routes>
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
-          {/* <PrivateRoute path="/dashboard" element={Dashboard} /> */}
-          <Route path="/dashboard" element={<Dashboard/>} />
-          {/* <Route path="/" element={<Login/>} /> */}
-          <Route path='/' element={<Homepage/>}/>
-          {/* <Route path="/dashboard" element={<DashboardPage/>} /> */}
-          <Route path="/" element={<Login/>} />
-
-        </Routes>
-      </PaymentProvider>
-    </>
+    <div>
+      
+    </div>
   )
 }
 
-export default App;
+export default App
